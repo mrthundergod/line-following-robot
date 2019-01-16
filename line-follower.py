595281b4d8dbae2a0880d59c80f5a66fdbcfddad
Aravind AJ <35934260+mrthundergod@users.noreply.github.com>
2019-01-16 16:52:41 +0530

    Added more descriptions

--- line-follower.py
+++ line-follower.py
@@ -1,12 +1,6 @@
-# Gregory Mueth
-# 2635999
-# EEL 4660 Robotics Systems
-# Fall 2017
-
-# line-follower.py
 
 # A line following robot for Robotics Systems final project
-# Coded using Python 2.7.13 and OpenCV 3.3 for a Raspberry Pi 3B running Raspbian
+# Coded using Python 3 and OpenCV 3.3 for a Raspberry Pi 3B running Raspbian
 # Uses Raspberry Pi camera module v2
 
 from picamera.array import PiRGBArray
@@ -42,31 +36,34 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# Create key to break for loop
 	key = cv2.waitKey(1) & 0xFF
 
-	# convert to grayscale, gaussian blur, and threshold
+	# convert to grayscale for easier processing.
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	#smoothen the image using Gaussian blur via(i/p img, (dimensions of kernal, must be odd and positive), standard_deviation)
 	blur = cv2.GaussianBlur(gray,(5,5),0)
+	# converts the image according to the threshold values, fourth arg reverses the colors since we need white obj in black bg for finding contours.
 	ret,thresh1 = cv2.threshold(blur,100,255,cv2.THRESH_BINARY_INV)
 
-	# Erode to eliminate noise, Dilate to restore eroded parts of image
+	# Erode to eliminate noise, by removing a bit of the edges of the image, like feathering.(img,kernal.iter)
 	mask = cv2.erode(thresh1, None, iterations=2)
+	#Dilate to restore eroded parts of image as eroding shrinks the image.(img,kernal.iter)
 	mask = cv2.dilate(mask, None, iterations=2)
-
-	# Find all contours in frame
+	
+	# this can be done in one step using opening.
+	# opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+	
+	# Find all contours in frame,(img.copy,relation_find,compress_stuff)
 	something, contours, hierarchy = cv2.findContours(mask.copy(),1,cv2.CHAIN_APPROX_NONE)
 
 	# Find x-axis centroid of largest contour and cut power to appropriate motor
 	# to recenter camera on centroid.
-	# This control algorithm was written referencing guide:
-		# Author: Einsteinium Studios
-		# Availability: http://einsteiniumstudios.com/beaglebone-opencv-line-following-robot.html
 	if len(contours) > 0:
 		# Find largest contour area and image moments
 		c = max(contours, key = cv2.contourArea)
 		M = cv2.moments(c)
 
-		# Find x-axis centroid using image moments
+		# Find x-axis centroid using image moments and convert them to integer. Centroid is given by the relations, Cx=[M10/M00] and Cy=[M01/M00]
 		cx = int(M['m10']/M['m00'])
-
+		# the width value is from 0 to 190 so lets say the road/path is in the middle 40-150.
 		if cx >= 150:
 			GPIO.output(12, GPIO.LOW)
 			GPIO.output(21, GPIO.HIGH)
@@ -84,9 +81,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	rawCapture.truncate(0)
 
+#Turn off motors
 GPIO.output(12, GPIO.LOW)
 GPIO.output(16, GPIO.LOW)
 GPIO.output(20, GPIO.LOW)
 GPIO.output(21, GPIO.LOW)
 
+#remove assigned values of pins
 GPIO.cleanup()
